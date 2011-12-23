4d99017e0f0fcc38858f68a0e1f232d3bba17284
Dmitry Deshevoy <mityada@gmail.com>
2011-12-24 02:45:05 +0400

    Removed everything except Vizi sources

--- ant.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if [%1] == [__CLASSPATH__] (
-    set classpath=%classpath%;%2
-) else (
-    set classpath=%JAVA_HOME%\lib\tools.jar
-    for %%a in (meta\bin\lib\*.jar) do call %0 __CLASSPATH__ %%a
-    call %java_home%\bin\java org.apache.tools.ant.Main %*
-)
--- meta/bin/lib/SchemaValidator/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set classpath=..\ant.jar;..\xercesImpl.jar;
-del ..\SchemaValidatorTask.jar
-del *.class
-call javac -deprecation SchemaValidatorTask.java ViziTools.java
-call jar -cf ../SchemaValidatorTask.jar *.class
